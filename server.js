91a8757a86f7e2ff07dff2226ac7c5214ffd3014
williamrankin <william.rankin01@gmail.com>
2024-01-06 18:41:54 -0700

    added express.js, added get for notes

---
+++ server.js
@@ -0,0 +1,33 @@
+// Import Express.js
+const express = require("express");
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require("path");
+// Initialize an instance of Express.js
+const app = express();
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
+const notesData = require("./db/db.json");
+
+// Static middleware pointing to the public folder
+app.use(express.static("public"));
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => res.json(notesData));
+
+app.post("/api/notes", (req, res) => {
+    res.json(`${req.method} request received`);
+});
+
+// listen() method is responsible for listening for incoming connections on the specified port
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
